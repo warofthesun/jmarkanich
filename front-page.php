cd05bb7edaac50651495abe2da05bc4caf798cc2
Dan Brown <fireflip@gmail.com>
2017-07-30 22:52:33 -0500

    Initial Commit

---
+++ front-page.php
@@ -0,0 +1,43 @@
+<?php get_header('front'); ?>
+<!--front-page-->
+			<div id="content">
+				<div class="hero_image"><a href="<?php the_field('hero_image_link'); ?>"><img src="<?php the_field('hero_image'); ?>"></a></div>
+				<div id="inner-content" class="wrap cf">
+
+						<main id="main" class="m-all t-3of3 d-7of7 cf" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
+
+							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+							<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+
+								<header class="article-header">
+
+									<h1 class="page-title" itemprop="headline"><?php the_title(); ?></h1>
+
+									<p class="byline vcard">
+										<?php printf( __( 'Posted', 'bonestheme').' <time class="updated" datetime="%1$s" itemprop="datePublished">%2$s</time> '.__( 'by',  'bonestheme').' <span class="author">%3$s</span>', get_the_time('Y-m-j'), get_the_time(get_option('date_format')), get_the_author_link( get_the_author_meta( 'ID' ) )); ?>
+									</p>
+
+								</header> <?php // end article header ?>
+
+								<section class="entry-content cf" itemprop="articleBody">
+									<?php	the_content(); ?>
+								</section> <?php // end article section ?>
+
+								<footer class="article-footer cf">
+
+								</footer>
+
+								<?php comments_template(); ?>
+
+							</article>
+
+							<?php endwhile; endif; ?>
+
+						</main>
+
+				</div>
+
+			</div>
+
+<?php get_footer(); ?>
