ad80c3725647bdfa6c4dcf6e1f20dd4448c8f83c
Dan Brown <fireflip@gmail.com>
2017-08-04 09:14:54 -0500

    Getting front page image nav in order

--- front-page.php
+++ front-page.php
@@ -3,21 +3,22 @@
 			<div id="content">
 				<div class="hero_image"><a href="<?php the_field('hero_image_link'); ?>"><img src="<?php the_field('hero_image'); ?>"></a></div>
 				<div id="inner-content" class="wrap cf">
-						<div class="d-1of3 m-all navigation_image">
-							<?php
-								$imageArray = get_field('first_navigation_image'); // Array returned by Advanced Custom Fields
-								$imageAlt = esc_attr($imageArray['alt']); // Grab, from the array, the 'alt'
-								$imageThumbURL = esc_url($imageArray['sizes']['front-page-navigation']); //grab from the array, the 'sizes', and from it, the 'thumbnail'
-							?>
-							<div>
+					<?php
+						$imageArray = get_field('first_navigation_image'); // Array returned by Advanced Custom Fields
+						$imageAlt = esc_attr($imageArray['alt']); // Grab, from the array, the 'alt'
+						$imageThumbURL = esc_url($imageArray['sizes']['front-page-navigation']); //grab from the array, the 'sizes', and from it, the 'thumbnail'
+					?>
+						<a href="#" class="d-1of3 m-all">
+
+							<div class="navigation_image">
 								<img src="<?php echo $imageThumbURL;?>">
 							</div>
-							<div>
+							<div class="navigation_text">
 								<?php echo the_field('first_navigation_text'); ?>
 							</div>
-						</div>
+						</a>
 
-						<div class="d-1of3 m-all navigation_image">
+						<a href="#" class="d-1of3 m-all navigation_image">
 							<?php
 								$imageArray = get_field('second_navigation_image'); // Array returned by Advanced Custom Fields
 								$imageAlt = esc_attr($imageArray['alt']); // Grab, from the array, the 'alt'
@@ -26,12 +27,12 @@
 							<div>
 								<img src="<?php echo $imageThumbURL;?>">
 							</div>
-							<div>
+							<div class="navigation_text">
 								<?php echo the_field('second_navigation_text'); ?>
 							</div>
 
-						</div>
-						<div class="d-1of3 m-all navigation_image">
+						</a>
+						<a class="d-1of3 m-all navigation_image">
 							<?php
 								$imageArray = get_field('third_navigation_image'); // Array returned by Advanced Custom Fields
 								$imageAlt = esc_attr($imageArray['alt']); // Grab, from the array, the 'alt'
@@ -40,10 +41,10 @@
 							<div>
 								<img src="<?php echo $imageThumbURL;?>">
 							</div>
-							<div>
+							<div class="navigation_text">
 								<?php echo the_field('third_navigation_text'); ?>
 							</div>
-						</div>
+						</a>
 				</div>
 
 			</div>
