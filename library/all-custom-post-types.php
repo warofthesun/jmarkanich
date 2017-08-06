a0d9d4b74cba48fccd14b39744f1281c37132867
Daniel Brown <danielbrown@Daniels-MBP.attlocal.net>
2017-08-06 15:50:57 -0500

    Create Testimonials Post Type and Archive Page

--- library/all-custom-post-types.php
+++ library/all-custom-post-types.php
@@ -105,7 +105,55 @@ function portfolio_post_type() {
 		)
 	);
 
-	
+	// Testimonials Type
+
+	function testimonial_post_type() {
+		// creating (registering) the custom type
+		register_post_type( 'testimonial_type', /* (http://codex.wordpress.org/Function_Reference/register_post_type) */
+			// let's now add all the options for this post type
+			array( 'labels' => array(
+				'name' => __( 'Testimonials', 'bonestheme' ), /* This is the Title of the Group */
+				'singular_name' => __( 'Testimonial', 'bonestheme' ), /* This is the individual type */
+				'all_items' => __( 'Testimonials', 'bonestheme' ), /* the all items menu item */
+				'add_new' => __( 'Add New', 'bonestheme' ), /* The add new menu item */
+				'add_new_item' => __( 'Add New Testimonial', 'bonestheme' ), /* Add New Display Title */
+				'edit' => __( 'Edit', 'bonestheme' ), /* Edit Dialog */
+				'edit_item' => __( 'Edit Testimonial', 'bonestheme' ), /* Edit Display Title */
+				'new_item' => __( 'New Testimonial', 'bonestheme' ), /* New Display Title */
+				'view_item' => __( 'View Testimonial', 'bonestheme' ), /* View Display Title */
+				'search_items' => __( 'Search Testimonials', 'bonestheme' ), /* Search Custom Type Title */
+				'not_found' =>  __( 'Nothing found in the Database.', 'bonestheme' ), /* This displays if there are no entries yet */
+				'not_found_in_trash' => __( 'Nothing found in Trash', 'bonestheme' ), /* This displays if there is nothing in the trash */
+				'parent_item_colon' => ''
+				), /* end of arrays */
+				'description' => __( 'This is the post type for testimonials', 'bonestheme' ), /* Custom Type Description */
+				'public' => true,
+				'publicly_queryable' => true,
+				'exclude_from_search' => false,
+				'show_ui' => true,
+				'query_var' => true,
+				'menu_position' => 8, /* this is what order you want it to appear in on the left hand side menu */
+				'menu_icon' => get_stylesheet_directory_uri() . '/library/images/custom-post-icon.png', /* the icon for the custom post type menu */
+				'rewrite'	=> array( 'slug' => 'testimonials', 'with_front' => false ), /* you can specify its url slug */
+				'has_archive' => 'testimonials', /* you can rename the slug here */
+				'capability_type' => 'post',
+				'hierarchical' => false,
+				/* the next one is important, it tells what's enabled in the post editor */
+				'supports' => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'trackbacks', 'custom-fields', 'comments', 'revisions', 'sticky')
+			) /* end of options */
+		); /* end of register post type */
+
+		/* this adds your post categories to your custom post type */
+		register_taxonomy_for_object_type( 'category', 'testimonial_type' );
+		/* this adds your post tags to your custom post type */
+		register_taxonomy_for_object_type( 'post_tag', 'testimonial_type' );
+
+	}
+
+		// adding the function to the Wordpress init
+		add_action( 'init', 'testimonial_post_type');
+
+
 
 
 ?>
