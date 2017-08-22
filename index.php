1fdff4307f0f8bce80a34f0fd812236d475a9139
Daniel Brown <danielbrown@Daniels-MBP.attlocal.net>
2017-08-21 20:55:25 -0500

    Update portfolio gallery thumbnail size

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 							<div>
 							   <?php the_post(); ?>
 								 <article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article">
-	 								<?php the_post_thumbnail('portfolio_gallery_thumbnail') ?>
+	 								<?php the_post_thumbnail('blog_hero') ?>
 	 								<header class="article-header">
 
 	 									<h1 class="h2 entry-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
@@ -42,11 +42,11 @@
 								<header class="article-header post-list">
 									<h1 class="h2 entry-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
 									<p class="byline entry-meta vcard">
-                    <?php printf( __( 'Posted', 'bonestheme' ).' %1$s %2$s',
+                    <?php printf( __( '', 'bonestheme' ).'%2$s | %1$s ',
                        								/* the time the post was published */
                        								'<time class="updated entry-time" datetime="' . get_the_time('Y-m-d') . '" itemprop="datePublished">' . get_the_time(get_option('date_format')) . '</time>',
                        								/* the author of the post */
-                       								'<span class="by">'.__( 'by', 'bonestheme').'</span> <span class="entry-author author" itemprop="author" itemscope itemptype="http://schema.org/Person">' . get_the_author_link( get_the_author_meta( 'ID' ) ) . '</span>'
+                       								'<span class="by">'.__( '', 'bonestheme').'</span> <span class="entry-author author" itemprop="author" itemscope itemptype="http://schema.org/Person">' . get_the_author_link( get_the_author_meta( 'ID' ) ) . '</span>'
                     							); ?>
 									</p>
 
