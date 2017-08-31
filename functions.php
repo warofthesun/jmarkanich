c988db2d110cb3b1931488249a8cdd767f432344
Daniel Brown <danielbrown@Daniels-MBP.attlocal.net>
2017-08-31 12:18:54 -0500

    Add Button style to Editor dropdown

--- functions.php
+++ functions.php
@@ -120,6 +120,49 @@ duplicate one of the lines in the array and name it according to your
 new image size.
 */
 
+/************* ADD ADDITIONAL STYLES TO EDITOR ******************/
+
+function wpb_mce_buttons_2($buttons) {
+    array_unshift($buttons, 'styleselect');
+    return $buttons;
+}
+add_filter('mce_buttons_2', 'wpb_mce_buttons_2');
+
+/*
+* Callback function to filter the MCE settings
+*/
+
+function my_mce_before_init_insert_formats( $init_array ) {
+
+// Define the style_formats array
+
+    $style_formats = array(
+/*
+* Each array child is a format with it's own settings
+* Notice that each array has title, block, classes, and wrapper arguments
+* Title is the label which will be visible in Formats menu
+* Block defines whether it is a span, div, selector, or inline style
+* Classes allows you to define CSS classes
+* Wrapper whether or not to add a new block-level element around any selected elements
+*/
+        array(
+            'title' => 'Button',
+            'block' => 'span',
+            'classes' => 'white-btn',
+            'wrapper' => true,
+
+        ),
+
+    );
+    // Insert the array, JSON ENCODED, into 'style_formats'
+    $init_array['style_formats'] = json_encode( $style_formats );
+
+    return $init_array;
+
+}
+// Attach callback to 'tiny_mce_before_init'
+add_filter( 'tiny_mce_before_init', 'my_mce_before_init_insert_formats' );
+
 /************* LIMIT POST FORMATS ******************/
 function ti_get_allowed_project_formats() {
 
